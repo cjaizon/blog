3f1b3197f80c17b7c38d79966f5e2a7368e48832
Jei Studio <jaizoncarlos@protonmail.com>
2021-09-14 13:42:45 -0300

    amost done

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const { DateTime } = require('luxon')
+
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addPassthroughCopy('./src/style.css')
+    eleventyConfig.addPassthroughCopy('./src/assets')
+
+    eleventyConfig.addFilter('postDate', (dateObj) =>
+        DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    )
+
+    return {
+        dir: {
+            input: 'src',
+            output: 'public',
+        },
+    }
+}
